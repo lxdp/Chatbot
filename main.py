8c70b219b83c0062e0b1d7f7931154a75c7b5dac
Lucas XD Pedersen <lucasxdpedersen@Larss-MacBook-Air-8.local>
2024-09-20 15:14:27 +0100

    Initial chatbot code

---
+++ main.py
@@ -0,0 +1,10 @@
+from chatterbot import ChatBot
+
+chatbot = ChatBot("Chatbot")
+exit_conditions = (":q", "quit", "exit")
+while True:
+    query = input("> ")
+    if query in exit_conditions:
+        break
+    else:
+        print(f"{chatbot.get_response(query)}")
