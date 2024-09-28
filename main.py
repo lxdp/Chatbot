7e325521dfd4bc667b06899200288042a7ba3250
Lucas XD Pedersen <lucasxdpedersen@Larss-MacBook-Air-8.local>
2024-09-28 01:35:45 +0100

    Improved so that it returns the verified answer

--- main.py
+++ main.py
@@ -36,8 +36,6 @@ def answer_questions(query):
     
     heading = soup.find('h1').get_text()
     
-    print(heading)
-    
     documents = [heading, query]
     
     vectorizer = TfidfVectorizer()
@@ -46,15 +44,33 @@ def answer_questions(query):
     cos_simularity = cosine_similarity(tfid_matrix[0:1], tfid_matrix[1:2])
     
     if cos_simularity >= 0.2:
-        div = soup.find('div', class_='s-prose js-post-body')
         
-        if div:
-            text = div.get_text(strip = True)
-    
-    if text:
-        return text
-    else:
-        return "We could not find what you are looking for"
+        verified_answer = soup.find('div', class_= 'js-accepted-answer-indicator flex--item fc-green-400 py6 mtn8')
+        
+        
+        if verified_answer:
+            container = verified_answer.find_parent('div', class_='answer js-answer accepted-answer js-accepted-answer')
+            
+            
+        if container:
+            
+            for remove in container.select('.user-details, .share, .footer, .post-signature'):
+                remove.decompose()
+                
+            removed_phrases = ["Share", "Improve this answer", "Follow", "Add a comment", "|"]
+            
+            answer = container.get_text(strip=True)
+            
+            for phrase in removed_phrases:
+                answer = answer.replace(phrase, '')
+            
+            answer = re.sub(r'^\d+', '', answer)  
+            answer = re.sub(r'\s{2,}', ' ', answer)
+            
+            return answer
+                
+        else:
+            return "There are no verified answers but we can give you some suggestions made"
     
 
 exit_conditions = (":q", "quit", "exit")
